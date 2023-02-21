0c770922d1f12e4ea6598bad201c80c748be15a5
MilaKumasinskaya <mila.kumasinskaya@mail.ru>
2023-02-21 22:16:57 +0300

    fixed task 13

--- Training JS/13. number_object_and_its_properties.js
+++ Training JS/13. number_object_and_its_properties.js
@@ -6,16 +6,18 @@ function whatNumberIsIt(n) {
     if (n == Number.MAX_VALUE) {
         return "Input number is Number.MAX_VALUE";
     }
-    if (n == 100) {
-        return "Input number is 100";
-    }
     if (n == Number.POSITIVE_INFINITY) {
         return "Input number is Number.POSITIVE_INFINITY";
     }
     if (n == Number.NEGATIVE_INFINITY){
         return "Input number is Number.NEGATIVE_INFINITY"
     }
-    else return "Input number is Number.NaN"
+    if (!Number.isNaN(n)){
+        return "Input number is " + n;
+    }
+    if (Number.isNaN(n)){
+        return "Input number is Number.NaN";
+    }
 
 
 }
